0e74f8e76594a9910c3ae307ab63426cb5a753fb
xavier.dutoit <xavier@tttp.eu>
2017-11-04 13:57:11 +0100

    pop up da mep

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ var uglify = require('gulp-uglify');
 var minify = require('gulp-minify-css');
 var sourcemaps = require('gulp-sourcemaps');
 var download = require("gulp-download");
+var xz = require("xz");
+var rename = require("gulp-rename");
+var decompression = new xz.Decompressor();
 
 var paths = {
   css: [
@@ -13,13 +16,14 @@ var paths = {
   ],
   scripts: [
     'node_modules/jquery/dist/jquery.js',
-    'node_modules/lodash/lodash.js',
+    'node_modules/blueimp-tmpl/js/tmpl.js',
+//	  'node_modules/lodash/lodash.js',
     'node_modules/d3/d3.js',
     'node_modules/crossfilter2/crossfilter.js',
     'node_modules/reductio/reductio.js',
     'node_modules/dc/dc.js',
     'node_modules/bootstrap/dist/js/bootstrap.js',
-    'node_modules/jquery-lazyload/jquery.lazyload.js',
+//    'node_modules/jquery-lazyload/jquery.lazyload.js',
     'node_modules/moment/moment.js'
   ]
 
@@ -27,6 +31,13 @@ var paths = {
 gulp.task('fetch', function() {
   download("http://parltrack.euwiki.org/dumps/ep_meps_current.json.xz")
     .pipe(gulp.dest('data'));
+});
+
+gulp.task('decompress', function() {
+  var fname="data/ep_meps_current.json";
+  var inFile = fs.createReadStream(fname + ".xz");
+  var outFile = fs.createWriteStream(fname);
+  inFile.pipe(decompression).pipe(outFile);
 });
 
 gulp.task('stylesheets', function() {
--- transform.js
+++ transform.js
@@ -18,10 +18,68 @@ function f(assembler) {
 }
 
 var total = 0;
+var committees = {};
+var groups = {};
+//epid,country,first_name,last_name,email,birthdate,gender,eugroup,party,phone,building,office,committee,substitute,delegation,twitter,tttpid,since
+
 var stream = StreamFilteredArray.make({
   objectFilter: f
 });
 var transform = s.Transform;
+
+var abbr= {"Committee of Inquiry to investigate alleged contraventions and maladministration in the application of Union law in relation to money laundering, tax avoidance and tax evasion":"LUXLEAK"};
+
+function transform(d) {
+//epid,country,first_name,last_name,email,birthdate,gender,eugroup,party,phone,building,office,committee,substitute,delegation,twitter,tttpid,since
+  function activeOnly(attr) {
+    var r = [];
+    if (!d[attr]) return;
+    d[attr].forEach(function(item) {
+      if (item.end !== '9999-12-31T00:00:00') return;
+      delete item.end;
+      item.start = item.start.replace("T00:00:00", "");
+      if (item.abbr == null) {
+        delete item.abbr;
+	item.name = abbr[item.Organization];
+        delete item.Organization;
+      } else {
+	item.name=item.abbr;
+        delete item.abbr;
+        delete item.Organization;
+      }
+      if (item.committee_id) delete item.committee_id;
+      r.push(item);
+    });
+    d[attr] = r;
+  }
+  delete d.changes;
+  delete d.assistants;
+  delete d._id;
+  delete d.meta.url;
+  delete d.Name.aliases;
+  delete d.Name.familylc;
+  delete d.Name.full;
+  delete d.Addresses.Postal;
+  delete d.Addresses.Strasbourg;
+  d.Addresses.Brussels.Office = d.Addresses.Brussels.Address.Office;
+  delete d.Addresses.Brussels.Address;
+  delete d.Addresses.Brussels.Fax;
+  delete d.Photo; //"http://www.europarl.europa.eu/mepphoto/{{d.UserID }}.jpg
+  delete d.activities; // stuff might be to be kept there
+  delete d["Declarations of Participation"];
+  delete d["Financial Declarations"];
+
+  if (d.Birth) d.Birth.date = d.Birth.date.replace("T00:00:00","");
+  delete d.Delegations;
+  //activeOnly("Delegations");
+  activeOnly("Committees");
+  activeOnly("Constituencies");
+  activeOnly("Groups");
+  if (Array.isArray(d.Groups))
+    delete d.Groups[0].Organization;
+  activeOnly("Staff");
+  return d;
+}
 
 function clean(d) {
   function activeOnly(attr) {
@@ -33,7 +91,11 @@ function clean(d) {
       item.start = item.start.replace("T00:00:00", "");
       if (item.abbr == null) {
         delete item.abbr;
+	item.name = abbr[item.Organization];
+        delete item.Organization;
       } else {
+	item.name=item.abbr;
+        delete item.abbr;
         delete item.Organization;
       }
       if (item.committee_id) delete item.committee_id;
@@ -42,14 +104,30 @@ function clean(d) {
     d[attr] = r;
   }
   delete d.changes;
+  delete d.assistants;
   delete d._id;
+  delete d.meta.url;
   delete d.Name.aliases;
+  delete d.Name.familylc;
   delete d.Name.full;
+  delete d.Addresses.Postal;
+  delete d.Addresses.Strasbourg;
+  d.Addresses.Brussels.Office = d.Addresses.Brussels.Address.Office;
+  delete d.Addresses.Brussels.Address;
+  delete d.Addresses.Brussels.Fax;
+  delete d.Photo; //"http://www.europarl.europa.eu/mepphoto/{{d.UserID }}.jpg
   delete d.activities; // stuff might be to be kept there
-  activeOnly("Delegations");
+  delete d["Declarations of Participation"];
+  delete d["Financial Declarations"];
+
+  if (d.Birth) d.Birth.date = d.Birth.date.replace("T00:00:00","");
+  delete d.Delegations;
+  //activeOnly("Delegations");
   activeOnly("Committees");
   activeOnly("Constituencies");
   activeOnly("Groups");
+  if (Array.isArray(d.Groups))
+    delete d.Groups[0].Organization;
   activeOnly("Staff");
   return d;
 }
@@ -61,19 +139,26 @@ var simp = through2({
   total++;
   if (total > 1)
     this.push(",\n");
-  this.push(JSON.stringify(clean(chunk.value)))
+  //this.push(JSON.stringify(clean(chunk.value))) // keeps a similar structure
+  this.push(JSON.stringify(transform(chunk.value))) //change keys and flatten the structure
 
   callback();
 });
 
-stream.output.pipe(simp);
-
-stream.output.on("end", function() {
+simp.on('end', () => {
   console.log("done" + total);
 });
+
+stream.output.on("end", function() {
+  simp.push("]");
+  simp.end();
+});
+stream.output.pipe(simp);
 
 let fname = path.join(__dirname, 'data/ep_meps_current.json');
 fs.createReadStream(fname).pipe(stream.input)
 var writer = fs.createWriteStream(path.join(__dirname, 'data/meps.json'));
+//var csv = fs.createWriteStream(path.join(__dirname, 'data/meps.tmp'));
 //simp.pipe(process.stdout);
+simp.push("[");
 simp.pipe(writer);
