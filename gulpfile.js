afa69521fbf955867c0511f0e6ea86ebe95e382c
xavier.dutoit <xavier@tttp.eu>
2017-11-04 02:30:14 +0100

    add icons + filter on committee

--- gulpfile.js
+++ gulpfile.js
@@ -3,31 +3,37 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var minify = require('gulp-minify-css');
 var sourcemaps = require('gulp-sourcemaps');
+var download = require("gulp-download");
 
 var paths = {
   css: [
-'node_modules/dc/dc.css',
-	  'node_modules/bootstrap/dist/css/bootstrap.css',
-	  'node_modules/bootstrap/dist/css/bootstrap-theme.css'
+    'node_modules/dc/dc.css',
+    'node_modules/bootstrap/dist/css/bootstrap.css',
+    'node_modules/bootstrap/dist/css/bootstrap-theme.css'
   ],
   scripts: [
-'node_modules/jquery/dist/jquery.js',
-'node_modules/lodash/lodash.js',
-'node_modules/d3/d3.js',
-'node_modules/crossfilter2/crossfilter.js',
-'node_modules/reductio/reductio.js',
-'node_modules/dc/dc.js',
-'node_modules/bootstrap/dist/js/bootstrap.js',
-'node_modules/jquery-lazyload/jquery.lazyload.js',
-'node_modules/moment/moment.js'
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/lodash/lodash.js',
+    'node_modules/d3/d3.js',
+    'node_modules/crossfilter2/crossfilter.js',
+    'node_modules/reductio/reductio.js',
+    'node_modules/dc/dc.js',
+    'node_modules/bootstrap/dist/js/bootstrap.js',
+    'node_modules/jquery-lazyload/jquery.lazyload.js',
+    'node_modules/moment/moment.js'
   ]
 
 };
+gulp.task('fetch', function() {
+  download("http://parltrack.euwiki.org/dumps/ep_meps_current.json.xz")
+    .pipe(gulp.dest('data'));
+});
+
 gulp.task('stylesheets', function() {
   // with sourcemaps all the way down 
   return gulp.src(paths.css)
-      .pipe(minify())
-      .pipe(concat('all.css'))
+    .pipe(minify())
+    .pipe(concat('all.css'))
     .pipe(gulp.dest('build/css'));
 });
 
@@ -35,10 +41,10 @@ gulp.task('scripts', function() {
   // with sourcemaps all the way down 
   return gulp.src(paths.scripts)
     .pipe(sourcemaps.init())
-      .pipe(uglify())
-      .pipe(concat('all.min.js'))
+    .pipe(uglify())
+    .pipe(concat('all.min.js'))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('build/js'));
 });
 
-gulp.task('default', ['scripts','stylesheets']);
+gulp.task('default', ['scripts', 'stylesheets']);
--- transform.js
+++ transform.js
@@ -1,38 +1,40 @@
 'use strict';
 
 const fs = require('fs');
+const util = require('util');
 const path = require('path');
-const s=require("stream");
+const s = require("stream");
+const through2 = require('through2')
+
 const StreamFilteredArray = require("stream-json/utils/StreamFilteredArray");
 
-function f(assembler){
-  // test only top-level objects in the array: 
-  if(assembler.stack.length == 2 && assembler.key === null){
-    // make a decision depending on a boolean property "active": 
-    if(assembler.current.hasOwnProperty("active")){
-      // "true" to accept, "false" to reject 
+function f(assembler) {
+  if (assembler.stack.length == 2 && assembler.key === null) {
+    if (assembler.current.hasOwnProperty("active")) {
       return assembler.current.active;
     }
   }
   // return undefined to indicate our uncertainty at this moment 
 }
- 
-var stream = StreamFilteredArray.make({objectFilter: f});
+
+var total = 0;
+var stream = StreamFilteredArray.make({
+  objectFilter: f
+});
 var transform = s.Transform;
 
-// Example of use: 
- 
-function clean (d) {
+function clean(d) {
   function activeOnly(attr) {
-    var r=[];
-    d[attr].forEach(function(item){
+    var r = [];
+    if (!d[attr]) return;
+    d[attr].forEach(function(item) {
       if (item.end !== '9999-12-31T00:00:00') return;
       delete item.end;
-      item.start = item.start.replace("T00:00:00","");
-      if (item.abbr == null){
+      item.start = item.start.replace("T00:00:00", "");
+      if (item.abbr == null) {
         delete item.abbr;
       } else {
-	delete item.Organization;
+        delete item.Organization;
       }
       if (item.committee_id) delete item.committee_id;
       r.push(item);
@@ -40,7 +42,10 @@ function clean (d) {
     d[attr] = r;
   }
   delete d.changes;
+  delete d._id;
   delete d.Name.aliases;
+  delete d.Name.full;
+  delete d.activities; // stuff might be to be kept there
   activeOnly("Delegations");
   activeOnly("Committees");
   activeOnly("Constituencies");
@@ -49,17 +54,26 @@ function clean (d) {
   return d;
 }
 
-stream.output.on("data", function(data){
-  console.log(clean(data.value));
-  writer
-process.exit();
+var simp = through2({
+  objectMode: true
+}, function(chunk, enc, callback) {
+  process.stdout.write(".");
+  total++;
+  if (total > 1)
+    this.push(",\n");
+  this.push(JSON.stringify(clean(chunk.value)))
+
+  callback();
 });
 
-stream.output.on("end", function(){
-  console.log("done");
+stream.output.pipe(simp);
+
+stream.output.on("end", function() {
+  console.log("done" + total);
 });
- 
+
 let fname = path.join(__dirname, 'data/ep_meps_current.json');
-fs.createReadStream(fname).pipe(stream.input);
-var writer = fs.createWriteStream(path.join(__dirname,'data/meps.json')); 
-
+fs.createReadStream(fname).pipe(stream.input)
+var writer = fs.createWriteStream(path.join(__dirname, 'data/meps.json'));
+//simp.pipe(process.stdout);
+simp.pipe(writer);
