d134fab5a7fddba1b0d7b7f1f9485261a484e5e5
yetzt <git@yetzt.me>
2022-08-19 23:31:42 +0200

    formatting

--- gulpfile.js
+++ gulpfile.js
@@ -42,14 +42,14 @@ const paths = {
 
 const download = exports.download = function download(done){
 	return gulp_download([
-		{ file:"eugroup.json", url: "https://www.epnewshub.eu/v1/contributor/?type=grouppress&pageSize=50&search-value=&search-type=contributor" },
+		{ file: "eugroup.json", url: "https://www.epnewshub.eu/v1/contributor/?type=grouppress&pageSize=50&search-value=&search-type=contributor" },
 		{ file: "meps_str.js", url: "https://www.europarl.europa.eu/hemicycle/js/meps_str.js" }, //used by mepid task to generate mepid.json
 		// { file: "mepid.json", url: "http://www.europarl.europa.eu/meps/en/mepquicksearch.html?term=" },
-		{ file:"extra_csv.csv", url: "https://raw.githubusercontent.com/eliflab/European-Parliament-Open-Data/master/meps_full_list_with_twitter_accounts.csv" },
-		{ file:"incoming.xml", url: "https://www.europarl.europa.eu/meps/en/incoming-outgoing/incoming/xml" },
-		{ file:"outgoing.xml", url: "https://www.europarl.europa.eu/meps/en/incoming-outgoing/outgoing/xml" },
-		{ file:"ep_meps_current.json.lz", url: "https://parltrack.org/dumps/ep_meps.json.lz" },
-		{ file:"epnewshub.json", url: "https://www.epnewshub.eu/v1/contributor/?type=mep&pageSize=1000&search-value=&search-type=contributor" },
+		{ file: "extra_csv.csv", url: "https://raw.githubusercontent.com/eliflab/European-Parliament-Open-Data/master/meps_full_list_with_twitter_accounts.csv" },
+		{ file: "incoming.xml", url: "https://www.europarl.europa.eu/meps/en/incoming-outgoing/incoming/xml" },
+		{ file: "outgoing.xml", url: "https://www.europarl.europa.eu/meps/en/incoming-outgoing/outgoing/xml" },
+		{ file: "ep_meps_current.json.lz", url: "https://parltrack.org/dumps/ep_meps.json.lz" },
+		{ file: "epnewshub.json", url: "https://www.epnewshub.eu/v1/contributor/?type=mep&pageSize=1000&search-value=&search-type=contributor" },
 	]).pipe(gulp.dest('data').on('end', function(){
 		done();
 	}));	
