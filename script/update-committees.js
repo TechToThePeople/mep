8833f6a9c7803815ee12bc53d022707bbe5aba05
yetzt <git@yetzt.me>
2022-08-26 15:35:25 +0200

    script to update comittees

---
+++ script/update-committees.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+
+const src = "https://www.europarl.europa.eu/committees/en/about/list-of-committees";
+const dest = path.resolve(__dirname,"../data/committees.json");
+
+const fetch = require("node-fetch");
+const cheerio = require("cheerio");
+
+const data = require(dest);
+
+const main = module.exports = async function main(fn) {
+	if (typeof fn !== "function") fn = function(err){ if (err) throw err; }; // callback substitute
+	
+	const res = await fetch(src)
+	const html = await res.text();
+	
+	const $ = cheerio.load(html);
+	
+	$(".erpl_badge-committee","#website-body").each(function(i,e){
+		e = $(e);
+		const k = e.text().trim();
+		const v = e.attr("title").trim().replace(/^(Special )?(Committee|Subcommittee) (of Inquiry )?(on|to) (the )?/g,''); // trim pompousness
+		
+		// show changes
+		if (!data.hasOwnProperty(k)) console.log("[update-committees] new: %s - %s", k, v);
+		else if (data[k] !== v) console.log("[update-committees] change: %s - \n←\t'%s'\n→\t'%s'", k, data[k], v);
+		
+		data[k] = v;
+	});
+	
+	// save
+	fs.writeFile(dest, JSON.stringify(data,null,"\t"), fn);
+
+};
+
+if (require.main === module) main();
